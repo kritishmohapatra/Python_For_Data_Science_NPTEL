50bc346d21ec06041a70a10856c66aad3ad05857
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-23 16:57:40 +0530

    "comments part done"

---
+++ comments.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 23 16:49:28 2025
+
+@author: kriti
+"""
+import math
+
+# area of circle
+
+dia=5
+#diameter
+print(4*math.pi*(5//2)**2)
+
+#%clear for clearing the console
+
+#delete del variable name
+
+
+#entire environment %reset
+
+print(dir(math))
+
