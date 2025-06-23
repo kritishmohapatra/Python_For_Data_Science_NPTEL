6a120fea4bf68d881b0d9cc38432c06c4020811d
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-23 17:11:06 +0530

    "variables and data types completed"

---
+++ variables_data_types.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 23 16:57:52 2025
+
+@author: kriti
+"""
+
+#assignment operator
+# variable name should not begin with number
+
+# camel case 
+# ageEmp, AgeEmp
+# Snake
+# age_emp
+# Age_emp
+
+# pasacl case
+# AgeEmp 
+
+phys, chem, math=80, 80, 80
+
+
+# data types
+# boolean=True and False 
+# integers =int
+# Complex=complex 
+# float=float 
+# string=str
+
+
+#Dynamically type 
+
+name="kritish"
+age=20
+height=19.6
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+
+
+ht=int(height)
+print(type(ht))
+
+
+salary="1"
+s_i=int(salary)
+# print(s_i) not gives error
