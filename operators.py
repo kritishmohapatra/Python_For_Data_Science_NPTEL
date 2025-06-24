11dafc4f1db770eea18181581137f4ee9707e71b
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-24 16:15:55 +0530

    "operators done"

---
+++ operators.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 24 15:58:31 2025
+
+@author: kriti
+"""
+
+#operators
+#arithmetic operator
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+
+# = assignment operator
+# += plus equal to  first adds 
+# -= minus equal to first 
+# *= multiply equal to 
+# /= divide equal to
+
+# > greater than 
+# >= greater than equal to 
+# <less than 
+# <= less than equal to 
+# == equal to 
+# != not equal to 
+
+# and logical and
+# or  logical or 
+# not logical not
+
+
+# | bitwise or 
+# & bitwise and 
+
+# bitwise and 
+# bitwise or 
+# relational 
+# logical not
+# logical and 
+# logical or 
